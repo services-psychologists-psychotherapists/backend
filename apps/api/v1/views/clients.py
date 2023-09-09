bac628c13c263cafca35b7c8275bcc82b5ffed16
Ruslan Atarov <ruslanatarov@gmail.com>
2023-09-09 14:15:57 +0300

    fixed api views and client registration

---
+++ apps/api/v1/views/clients.py
@@ -0,0 +1,47 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import views, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from apps.clients.models import Client
+from apps.clients.selectors import get_client
+from apps.clients.services import create_client, update_client
+
+from ..permissions import IsClientOnly
+from ..serializers.clients import CreateUserSerializer, ClientSerializer
+
+
+class CreateClientApiView(views.APIView):
+    """Создание нового клиента на сайте. Доступ - любой пользователь."""
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        """Создание нового клиента на сайте."""
+        user_serializer = CreateUserSerializer(data=request.data)
+        user_serializer.is_valid(raise_exception=True)
+        client_serializer = ClientSerializer(data=request.data)
+        client_serializer.is_valid(raise_exception=True)
+        client = create_client(user_serializer.validated_data,
+                               client_serializer.validated_data)
+        return Response(ClientSerializer(client).data,
+                        status=status.HTTP_201_CREATED)
+
+
+class ClientApiView(views.APIView):
+    """Просмотр и изменение данных клиентского профиля пользователя."""
+    permission_classes = (IsClientOnly,)
+
+    def get(self, request):
+        """Просмотр клиентского профиля."""
+        client = get_client(request.user)
+        return Response(ClientSerializer(client).data,
+                        status=status.HTTP_200_OK)
+
+    def put(self, request):
+        """Изменение клиентского профиля."""
+        client = get_object_or_404(Client, user=request.user)
+        serializer = ClientSerializer(client, request.data)
+        serializer.is_valid(raise_exception=True)
+        client = update_client(client, serializer.validated_data)
+        return Response(ClientSerializer(client).data,
+                        status=status.HTTP_200_OK)
