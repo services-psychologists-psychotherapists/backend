6e3e7cdc26613ee5dcd321389a92d869a1ed9bac
Ruslan Atarov <ruslanatarov@gmail.com>
2023-09-10 15:24:59 +0300

    added mail service

---
+++ apps/core/utils.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import login, logout, user_logged_in, user_logged_out
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+
+def encode_uid(pk):
+    return force_str(urlsafe_base64_encode(force_bytes(pk)))
