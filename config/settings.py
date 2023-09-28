79cf7be28101086991d5b46ba9bab4ef5efbf9d0
Владислав Нестеров <nestern8@gmail.com>
2023-09-29 02:09:52 +0700

    added settings certbot

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sharewithme.acceleratorpracticum.ru', '185.93.108.168', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
