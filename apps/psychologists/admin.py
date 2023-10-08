df618e420dda8ff733f64ac355de73db1a23f5b7
verafadeeva <lunatik.yar@gmail.com>
2023-10-08 16:35:22 +0300

    Add sending email to psychologist where acc is verified

--- apps/psychologists/admin.py
+++ apps/psychologists/admin.py
@@ -1,27 +1,36 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import ngettext
 
 from apps.psychologists import models
+from apps.psychologists.threads import PsychoActivationEmailThread
 
 
 @admin.register(models.Institute)
 class InstituteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_higher')
-    empty_value_display = '-пусто-'
-    search_fields = ('title__startswith', )
+    list_display = ("id", "title", "is_higher")
+    empty_value_display = "-пусто-"
+    search_fields = ("title__startswith",)
 
 
 @admin.register(models.Theme)
 class ThemeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', )
-    empty_value_display = '-пусто-'
-    search_fields = ('title__startswith', )
+    list_display = (
+        "id",
+        "title",
+    )
+    empty_value_display = "-пусто-"
+    search_fields = ("title__startswith",)
 
 
 @admin.register(models.Approach)
 class ApproachAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', )
-    empty_value_display = '-пусто-'
-    search_fields = ('title__startswith', )
+    list_display = (
+        "id",
+        "title",
+    )
+    empty_value_display = "-пусто-"
+    search_fields = ("title__startswith",)
 
 
 class PsychoEducationInline(admin.TabularInline):
@@ -37,18 +46,50 @@ class ServiceInline(admin.TabularInline):
 @admin.register(models.ProfilePsychologist)
 class ProfilePsychologistAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ('user', 'is_verified')}),
-        ('Общая информация', {'fields': ('first_name', 'last_name',
-                                         'birthday', 'gender', 'phone_number',
-                                         'about', 'started_working',
-                                         )
-                              }
-         ),
-        ('О работе',         {'fields': ['themes', 'approaches']}),
+        (None, {"fields": ("user", "is_verified")}),
+        (
+            "Общая информация",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "birthday",
+                    "gender",
+                    "phone_number",
+                    "about",
+                    "started_working",
+                )
+            },
+        ),
+        ("О работе", {"fields": ["themes", "approaches"]}),
     ]
     inlines = (PsychoEducationInline, ServiceInline)
-    list_display = ('user', 'first_name', 'last_name', 'birthday',
-                    'gender', 'is_verified', 'speciality')
-    empty_value_display = '-пусто-'
-    list_filter = ('is_verified', )
-    search_fields = ('last_name__startswith', )
+    list_display = (
+        "user",
+        "first_name",
+        "last_name",
+        "birthday",
+        "gender",
+        "is_verified",
+        "speciality",
+    )
+    empty_value_display = "-пусто-"
+    list_filter = ("is_verified",)
+    search_fields = ("last_name__startswith",)
+    actions = ("send_activation_email",)
+
+    @admin.action(description="Отправить письмо")
+    def send_activation_email(self, request, queryset):
+        for psychologist in queryset:
+            PsychoActivationEmailThread(request, psychologist).start()
+
+        self.message_user(
+            request,
+            ngettext(
+                "%d письмо было успешно отправлено.",
+                "%d писем были успешно отправлены.",
+                len(queryset),
+            )
+            % len(queryset),
+            messages.SUCCESS,
+        )
