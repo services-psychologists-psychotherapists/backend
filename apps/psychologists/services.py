66e7c4cb6ccfd6bd99d93fe3da5c24626a37ed62
verafadeeva <lunatik.yar@gmail.com>
2023-10-13 10:56:25 +0300

    Fix services black formatter

--- apps/psychologists/services.py
+++ apps/psychologists/services.py
@@ -2,22 +2,30 @@ from datetime import date
 from typing import OrderedDict
 
 from django.db import transaction
-from django.core.exceptions import (ValidationError as DjangoValidationError,
-                                    ObjectDoesNotExist)
+from django.core.exceptions import (
+    ValidationError as DjangoValidationError,
+    ObjectDoesNotExist,
+)
 from django.http import HttpRequest
 from rest_framework import exceptions
 
 from apps.core.models import UploadFile
-from apps.psychologists.models import (ProfilePsychologist, Institute,
-                                       Service, Theme, Approach)
+from apps.psychologists.models import (
+    ProfilePsychologist,
+    Institute,
+    Service,
+    Theme,
+    Approach,
+)
 from apps.psychologists.threads import PsychoConfirmationFormEmailThread
 from apps.users.models import CustomUser
 
 
 @transaction.atomic
-def create_psychologist(validated_data: OrderedDict,
-                        request: HttpRequest,
-                        ) -> tuple[CustomUser, ProfilePsychologist]:
+def create_psychologist(
+    validated_data: OrderedDict,
+    request: HttpRequest,
+) -> tuple[CustomUser, ProfilePsychologist]:
     """
     psychologist_data содержит поля, обязательные для заполнения плюс:
     validated_data = {
@@ -34,7 +42,7 @@ def create_psychologist(validated_data: OrderedDict,
         "experience": int,
     }
     """
-    email = validated_data.pop('email')
+    email = validated_data.pop("email")
 
     user = CustomUser.objects.create_user(
         is_client=False,
@@ -50,27 +58,24 @@ def create_psychologist(validated_data: OrderedDict,
 
 
 @transaction.atomic
-def create_profile(user: CustomUser,
-                   psychologist_data: OrderedDict,
-                   ) -> ProfilePsychologist:
-    themes = get_themes(psychologist_data.pop('themes'))
+def create_profile(
+    user: CustomUser,
+    psychologist_data: OrderedDict,
+) -> ProfilePsychologist:
+    themes = get_themes(psychologist_data.pop("themes"))
 
-    approaches = get_or_create_approaches(psychologist_data.pop('approaches'))
+    approaches = get_or_create_approaches(psychologist_data.pop("approaches"))
 
-    institutes = get_or_create_education(
-        psychologist_data.pop('institutes'), flag=True
-    )
+    institutes = get_or_create_education(psychologist_data.pop("institutes"), flag=True)
 
     courses = []
-    if 'courses' in psychologist_data.keys():
-        courses = get_or_create_education(
-            psychologist_data.pop('courses'), flag=False
-        )
+    if "courses" in psychologist_data.keys():
+        courses = get_or_create_education(psychologist_data.pop("courses"), flag=False)
 
-    price = psychologist_data.pop('price')
+    price = psychologist_data.pop("price")
 
-    experience = psychologist_data.pop('experience')
-    psychologist_data['started_working'] = count_started_working(experience)
+    experience = psychologist_data.pop("experience")
+    psychologist_data["started_working"] = count_started_working(experience)
 
     try:
         psychologist = ProfilePsychologist.objects.create(
@@ -83,10 +88,8 @@ def create_profile(user: CustomUser,
     psychologist.themes.add(*themes)
     psychologist.approaches.add(*approaches)
     for data in institutes + courses:
-        education = data.pop('institute')
-        psychologist.education.add(
-            education, through_defaults=data
-        )
+        education = data.pop("institute")
+        psychologist.education.add(education, through_defaults=data)
 
     create_service(psychologist, price)
 
@@ -94,39 +97,35 @@ def create_profile(user: CustomUser,
 
 
 @transaction.atomic
-def update_psychologist(instance: ProfilePsychologist,
-                        data: OrderedDict
-                        ) -> ProfilePsychologist:
+def update_psychologist(
+    instance: ProfilePsychologist, data: OrderedDict
+) -> ProfilePsychologist:
     """
     Образование можно только добавлять, предыдущий set не меняется.
     """
 
-    if 'themes' in data:
-        themes = get_themes(data.pop('themes'))
+    if "themes" in data:
+        themes = get_themes(data.pop("themes"))
         instance.themes.set(themes)
-    if 'approaches' in data:
-        approaches = get_or_create_approaches(data.pop('approaches'))
+    if "approaches" in data:
+        approaches = get_or_create_approaches(data.pop("approaches"))
         instance.approaches.set(approaches)
     institutes = []
-    if 'institutes' in data:
-        institutes = get_or_create_education(data.pop('institutes'),
-                                             flag=True)
+    if "institutes" in data:
+        institutes = get_or_create_education(data.pop("institutes"), flag=True)
     courses = []
-    if 'courses' in data:
-        courses = get_or_create_education(data.pop('courses'),
-                                          flag=False)
-    if 'price' in data:
-        update_service(instance, data.pop('price'))
+    if "courses" in data:
+        courses = get_or_create_education(data.pop("courses"), flag=False)
+    if "price" in data:
+        update_service(instance, data.pop("price"))
 
     for key, value in data.items():
         setattr(instance, key, value)
     instance.save()
 
     for data in institutes + courses:
-        education = data.pop('institute')
-        instance.education.add(
-            education, through_defaults=data
-        )
+        education = data.pop("institute")
+        instance.education.add(education, through_defaults=data)
 
     return instance
 
@@ -147,9 +146,7 @@ def get_themes(iterable: list[OrderedDict]) -> list[Theme]:
     output = []
     for data in iterable:
         try:
-            obj = Theme.objects.get(
-                title=data['title'].title()
-            )
+            obj = Theme.objects.get(title=data["title"].title())
             output.append(obj)
         except ObjectDoesNotExist:
             pass
@@ -161,16 +158,15 @@ def get_or_create_approaches(iterable: list[OrderedDict]) -> list[Approach]:
     ("approaches"): [{"title": str}].
     """
     for i, data in enumerate(iterable):
-        obj, _ = Approach.objects.get_or_create(
-            title=data['title'].title()
-        )
+        obj, _ = Approach.objects.get_or_create(title=data["title"].title())
         iterable[i] = obj
     return iterable
 
 
-def get_or_create_education(iterable: list[OrderedDict],
-                            flag: bool,
-                            ) -> list[OrderedDict]:
+def get_or_create_education(
+    iterable: list[OrderedDict],
+    flag: bool,
+) -> list[OrderedDict]:
     """
     ("institutes" | "courses"): [
             {"title": str,
@@ -181,23 +177,24 @@ def get_or_create_education(iterable: list[OrderedDict],
         ]
     """
     for data in iterable:
-        title = data.pop('title')
+        title = data.pop("title")
         education, _ = Institute.objects.get_or_create(
             title=title,
             is_higher=flag,
         )
-        data['institute'] = education
-        document = data.pop('document')
+        data["institute"] = education
+        document = data.pop("document")
         file = UploadFile.objects.get(id=document)
-        data['document'] = file
+        data["document"] = file
     return iterable
 
 
-def create_service(psychologist: ProfilePsychologist,
-                   price: int,
-                   type: Service.Type = Service.Type.NOMATTER,
-                   format: Service.Format = Service.Format.ONLINE,
-                   ) -> Service:
+def create_service(
+    psychologist: ProfilePsychologist,
+    price: int,
+    type: Service.Type = Service.Type.NOMATTER,
+    format: Service.Format = Service.Format.ONLINE,
+) -> Service:
     """
     Создает Service
     """
@@ -210,11 +207,12 @@ def create_service(psychologist: ProfilePsychologist,
     return service
 
 
-def update_service(psychologist: ProfilePsychologist,
-                   price: int,
-                   type: Service.Type = Service.Type.NOMATTER,
-                   format: Service.Format = Service.Format.ONLINE,
-                   ) -> Service:
+def update_service(
+    psychologist: ProfilePsychologist,
+    price: int,
+    type: Service.Type = Service.Type.NOMATTER,
+    format: Service.Format = Service.Format.ONLINE,
+) -> Service:
     """
     Изменяет Service
     """
