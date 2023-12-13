b4c26b889ce8555bfeaa1e3388d47f13d59628fb
Ruslan Atarov <ruslanatarov@gmail.com>
2023-12-13 16:09:52 +0300

    replaced put in clients/me with patch method

--- apps/api/v1/views/clients.py
+++ apps/api/v1/views/clients.py
@@ -4,15 +4,22 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
 from ..permissions import IsClientOnly
-from ..serializers.clients import (ClientSerializer, CreateClientSerializer,
-                                   UserSerializer)
+from ..serializers.clients import (
+    ClientSerializer,
+    CreateClientSerializer,
+    UserSerializer,
+)
 
 
-@method_decorator(name='post', decorator=swagger_auto_schema(
-    responses={200: UserSerializer, 400: 'Bad request'}
-))
+@method_decorator(
+    name="post",
+    decorator=swagger_auto_schema(
+        responses={200: UserSerializer, 400: "Bad request"}
+    ),
+)
 class CreateClientView(generics.CreateAPIView):
     """Создание пользователя-клиента и профиля клиента."""
+
     serializer_class = CreateClientSerializer
     permission_classes = (AllowAny,)
 
@@ -22,9 +29,10 @@ class ClientView(generics.RetrieveUpdateAPIView):
     Просмотр и изменение профиля клиента.
     Поле "avatar" передается в формате base64.
     """
+
     serializer_class = ClientSerializer
     permission_classes = (IsClientOnly,)
-    http_method_names = ['get', 'put']
+    http_method_names = ["get", "patch"]
 
     def get_object(self):
         return self.request.user.client
