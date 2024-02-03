7c969eb75047cfba379bdcf4a4e81b5fee86858a
Ruslan Atarov <ruslanatarov@gmail.com>
2024-02-03 14:35:12 +0300

    fixed list all slots in psycho area

--- apps/session/selectors.py
+++ apps/session/selectors.py
@@ -12,3 +12,9 @@ def get_all_free_slots_by_user(user: CustomUser) -> QuerySet:
     return psycho.slots.filter(is_free=True).select_related(
         "session", "session__client"
     )
+
+
+def get_all_slots_by_user(user: CustomUser) -> QuerySet:
+    """Возвращает все слоты психолога."""
+    psycho = get_object_or_404(ProfilePsychologist, user=user)
+    return psycho.slots.select_related("session", "session__client")
